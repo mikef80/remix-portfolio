79fd9e0d6b26135eab0f6be1e13735407c3c958f
Mike <mike@mike-francis.org>
2024-06-21 21:51:14 +0100

    new scroll arrow added

--- app/components/DownArrow.tsx
+++ app/components/DownArrow.tsx
@@ -3,8 +3,8 @@ const DownArrow = ({ strokeWidth = 2,stroke='#393939' }) => (
     width='24'
     height='24'
     xmlns='http://www.w3.org/2000/svg'
-    fill-rule='evenodd'
-    clip-rule='evenodd'
+    fillRule='evenodd'
+    clipRule='evenodd'
     stroke={stroke}
     strokeWidth={strokeWidth}>
     <path d='M23.245 4l-11.245 14.374-11.219-14.374-.781.619 12 15.381 12-15.391-.755-.609z' />
