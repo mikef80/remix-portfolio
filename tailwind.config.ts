9b41152c661d51ddc0b85dff35ae3aeef931deeb
Mike <mike@mike-francis.org>
2024-06-21 22:32:47 +0100

    updated fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,12 @@ import type { Config } from "tailwindcss";
 export default {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        raleway: "Raleway",
+        roboto: "Roboto",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
