5cdc31f38cfcde7f28750283c9009d6f02d5fd62
Mike <mike@mike-francis.org>
2024-07-04 21:33:48 +0100

    Content update

--- app/components/Content.tsx
+++ app/components/Content.tsx
@@ -1,7 +1,7 @@
 import { ReactNode, FC } from 'react';
 
 const Content: FC<{ children: ReactNode }> = ({ children }) => {
-  return <div className="p-3 text-justify">{children}</div>;
+  return <div className="p-6 text-justify">{children}</div>;
 };
 
 export default Content;
