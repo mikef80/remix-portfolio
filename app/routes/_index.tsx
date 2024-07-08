3ecc9c7e9aacde162b44eec8dbf9a171321d6ab6
Mike <mike@mike-francis.org>
2024-07-08 23:18:04 +0100

    started journey visuals

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -5,6 +5,7 @@ import Content from "~/components/Content";
 import Hero from "~/components/Hero";
 import Navbar from "~/components/Navbar";
 import SkipLink from "~/components/SkipLink";
+import Timeline from "~/components/Timeline";
 
 export const meta: MetaFunction = () => {
   return [{ title: "Mike Francis - Portfolio" }, { name: "description", content: "Welcome to my Portfolio!" }];
@@ -20,6 +21,7 @@ export default function Index() {
       </header>
       <Hero />
       <main id='main' className='scroll-mt-20'>
+          <Timeline/>
         <Content>
           <About />
         </Content>
