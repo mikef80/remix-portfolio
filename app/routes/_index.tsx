adea5d65ab9e37c461eaa453a6dcdec0d210b89c
Mike <mike@mike-francis.org>
2024-07-29 21:12:09 +0100

    parking whilst i think about the layout

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -5,10 +5,12 @@ import Content from "~/components/Content";
 import Hero from "~/components/Hero";
 import Navbar from "~/components/Navbar";
 import SkipLink from "~/components/SkipLink";
-import Timeline from "~/components/Timeline";
 
 export const meta: MetaFunction = () => {
-  return [{ title: "Mike Francis - Portfolio" }, { name: "description", content: "Welcome to my Portfolio!" }];
+  return [
+    { title: "Mike Francis - Portfolio" },
+    { name: "description", content: "Welcome to my Portfolio!" },
+  ];
 };
 
 export default function Index() {
@@ -21,7 +23,6 @@ export default function Index() {
       </header>
       <Hero />
       <main id='main' className='scroll-mt-20'>
-          <Timeline/>
         {/* <Content>
           <About />
         </Content> */}
--- app/routes/history.tsx
+++
@@ -1,7 +0,0 @@
-import Timeline from "~/components/Timeline";
-
-const history = () => {
-  return <Timeline />;
-};
-
-export default history;
