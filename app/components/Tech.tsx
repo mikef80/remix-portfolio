77494f8f23a5fc674ee7932026b69b15a829604f
Mike <mike@mike-francis.org>
2024-09-17 22:00:59 +0100

    uuid completed. this has continued into project mapping

--- app/components/Tech.tsx
+++ app/components/Tech.tsx
@@ -1,93 +1,80 @@
 import Content from "./Content";
+import { v4 as uuidv4 } from "uuid";
+
+const techs = [
+  {
+    name: "HTML",
+    filename: "html5",
+    url: "https://developer.mozilla.org/en-US/docs/Web/HTML",
+  },
+  {
+    name: "CSS",
+    filename: "css3",
+    url: "https://developer.mozilla.org/en-US/docs/Web/CSS",
+  },
+  {
+    name: "JavaScript",
+    filename: "javascript",
+    url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+  },
+  {
+    name: "React",
+    filename: "react",
+    url: "https://reactjs.org/",
+  },
+  {
+    name: "Redux",
+    filename: "redux",
+    url: "https://redux.js.org/",
+  },
+  {
+    name: "Node.js",
+    filename: "nodejs",
+    url: "https://nodejs.org/",
+  },
+  {
+    name: "Express",
+    filename: "express",
+    url: "https://expressjs.com/",
+  },
+  {
+    name: "PostgreSQL",
+    filename: "postgresql",
+    url: "https://www.postgresql.org/",
+  },
+  {
+    name: "Tailwind CSS",
+    filename: "tailwindcss",
+    url: "https://tailwindcss.com/",
+  },
+  {
+    name: "PHP",
+    filename: "php",
+    url: "https://www.php.net/",
+  },
+  {
+    name: "WordPress",
+    filename: "wordpress",
+    url: "https://wordpress.org/",
+  },
+  {
+    name: "React Native",
+    filename: "react",
+    url: "https://reactnative.dev/",
+  },
+  {
+    name: "Git",
+    filename: "git",
+    url: "https://git-scm.com/",
+  },
+  {
+    name: "Postman",
+    filename: "postman",
+    url: "https://www.postman.com/",
+  },
+].map((tech) => ({ ...tech, uuid: uuidv4() }));
 
 const Tech = () => {
-  const techs = [
-    {
-      name: "HTML",
-      filename: "html5",
-      url: "https://developer.mozilla.org/en-US/docs/Web/HTML",
-      uuid: "cc14cc11-fdec-4d54-aea3-9c2f5910ab59",
-    },
-    {
-      name: "CSS",
-      filename: "css3",
-      url: "https://developer.mozilla.org/en-US/docs/Web/CSS",
-      uuid: "114fcf18-958f-4106-9404-10b86cf5d0e0",
-    },
-    {
-      name: "JavaScript",
-      filename: "javascript",
-      url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
-      uuid: "eb5d253c-48b3-4d9d-9a9d-c280645dfc41",
-    },
-    {
-      name: "React",
-      filename: "react",
-      url: "https://reactjs.org/",
-      uuid: "c9594d27-1e4e-481a-b1de-fb35d8997853",
-    },
-    {
-      name: "Redux",
-      filename: "redux",
-      url: "https://redux.js.org/",
-      uuid: "0155c63c-6f50-4217-8d4a-1e80fc14f93c",
-    },
-    {
-      name: "Node.js",
-      filename: "nodejs",
-      url: "https://nodejs.org/",
-      uuid: "e1e8637a-0ff8-4c4f-aed4-6c0c3ffa36b7",
-    },
-    {
-      name: "Express",
-      filename: "express",
-      url: "https://expressjs.com/",
-      uuid: "0a238e20-7e9f-4653-8d3c-cd19e074a75b",
-    },
-    {
-      name: "PostgreSQL",
-      filename: "postgresql",
-      url: "https://www.postgresql.org/",
-      uuid: "dd5b7c96-ff54-4244-b35f-f44ff359e7d1",
-    },
-    {
-      name: "Tailwind CSS",
-      filename: "tailwindcss",
-      url: "https://tailwindcss.com/",
-      uuid: "2203e932-3a6f-4da6-bdcb-3744f7af863b",
-    },
-    {
-      name: "PHP",
-      filename: "php",
-      url: "https://www.php.net/",
-      uuid: "feb5e262-77c1-4d3e-806c-877b7872b34b",
-    },
-    {
-      name: "WordPress",
-      filename: "wordpress",
-      url: "https://wordpress.org/",
-      uuid: "50636fa1-7f53-406b-8fb8-e4e15d89fbfa",
-    },
-    {
-      name: "React Native",
-      filename: "react",
-      url: "https://reactnative.dev/",
-      uuid: "983cc057-3abe-421f-8128-ca1da6162d9c",
-    },
-    {
-      name: "Git",
-      filename: "git",
-      url: "https://git-scm.com/",
-      uuid: "71b8eb31-5fe2-4b27-8dcf-888945b580f1",
-    },
-    {
-      name: "Postman",
-      filename: "postman",
-      url: "https://www.postman.com/",
-      uuid: "d1814109-fbf1-4a58-9d95-17c1825f6b19",
-    },
-  ];
-
   return (
     <div id='tech' className='scroll-mt-[5rem] bg-gray-800 text-white p-6'>
       <Content>
@@ -97,21 +84,19 @@ const Tech = () => {
           in a professional or educational setting.
         </p>
         <ul className='flex flex-wrap justify-center gap-2'>
-          {techs.map((tech) => {
-            return (
-              <li className='p-2' key={tech.uuid}>
-                {/* https://devicon.dev/ */}
-                <a href={tech.url} target='blank' className='flex flex-col items-center'>
-                  <img
-                    src={`https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/${tech.filename}/${tech.filename}-original.svg`}
-                    height='48'
-                    width='48'
-                  />
-                  <label htmlFor={tech.filename}>{tech.name}</label>
-                </a>
-              </li>
-            );
-          })}
+          {techs.map((tech) => (
+            <li className='p-2' key={tech.uuid}>
+              <a href={tech.url} target='blank' className='flex flex-col items-center'>
+                <img
+                  src={`https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/${tech.filename}/${tech.filename}-original.svg`}
+                  height='48'
+                  width='48'
+                  alt={tech.name}
+                />
+                <label htmlFor={tech.filename}>{tech.name}</label>
+              </a>
+            </li>
+          ))}
         </ul>
       </Content>
     </div>
