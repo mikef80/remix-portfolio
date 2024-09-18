3490ef18aa3286c116a2e260760aeb7096acc759
Mike <mike@mike-francis.org>
2024-09-18 16:48:18 +0100

    fix key errors

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -11,7 +11,7 @@ const Projects = () => {
           A selection of projects, both from self-driven and from my bootcamp.
         </p>
         <ul className='flex flex-wrap justify-center gap-2'>
-          <li className='p-2' key=''>
+          <li className='p-2'>
             <a href='' target='blank' className='flex flex-col items-center'>
               <img src='https://picsum.photos/800/450' alt='' />
               <h2 className='text-2xl'>Project</h2>
@@ -25,17 +25,46 @@ const Projects = () => {
               <div className='flex gap-2'>
                 <img
                   src={Code}
-                  style={{
+                  /* style={{
                     filter: "drop-shadow(0px 1000px 0 white)",
                     transform: "translateY(-1000px)",
-                  }}
+                  }} */
                 />
                 <img
                   src={Web}
-                  style={{
+                  /* style={{
                     filter: "drop-shadow(0px 1000px 0 white)",
                     transform: "translateY(-1000px)",
-                  }}
+                  }} */
+                />
+              </div>
+            </a>
+          </li>
+          <li className='p-2'>
+            <a href='' target='blank' className='flex flex-col items-center'>
+              <img src='https://picsum.photos/800/450' alt='' />
+              <h2 className='text-2xl'>Project</h2>
+              <p>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+                praesentium officia iusto dolorem ipsum quisquam expedita minima fugiat
+                excepturi veniam laborum, labore debitis rerum a soluta optio. Dicta deserunt
+                exercitationem iusto perferendis nam consequatur doloribus quas sunt quos at
+                voluptas rem dolores ut cumque, impedit, sint minus eaque architecto rerum?
+              </p>
+              <div className='flex gap-2'>
+                <img
+                  src={Code}
+                  /* style={{
+                    filter: "drop-shadow(0px 1000px 0 white)",
+                    transform: "translateY(-1000px)",
+                  }} */
+                />
+                <img
+                  src={Web}
+                  /* style={{
+                    filter: "drop-shadow(0px 1000px 0 white)",
+                    transform: "translateY(-1000px)",
+                  }} */
                 />
               </div>
             </a>
