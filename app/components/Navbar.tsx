de5b9c569c7a794d2e455d170c9da1134f1d8a08
Mike <mike@mike-francis.org>
2024-09-27 22:55:57 +0100

    db update successful. Need to work on blending login and other functions into functionality of the site

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { Link } from "@remix-run/react";
+import { Form, Link } from "@remix-run/react";
 import { MouseEventHandler, useEffect, useState } from "react";
 import RenderNavLink from "./RenderNavLink";
 
