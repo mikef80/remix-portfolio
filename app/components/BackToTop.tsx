95f59c0244c5f2fa08e3ad1329e61d0fd9a8fe9a
Mike <mike@mike-francis.org>
2024-10-05 21:24:53 +0100

    fixed return to top arrow positioning

--- app/components/BackToTop.tsx
+++ app/components/BackToTop.tsx
@@ -23,13 +23,16 @@ const BackToTop = () => {
 
   return (
     <Link
-      id='back-to-top'
-      to='#'
-      className={`border-white bg-gray-800 text-white fixed -right-[1%] bottom-[1%] -translate-x-1/2 p-2 border-2 rounded-md z-10 m-2 transition-opacity ease-in-out duration-300 ${
-        showLink ? "opacity-100 pointer-events-auto" : "opacity-0 pointer-events-none"
+      id="back-to-top"
+      to="#"
+      className={`border-white bg-gray-800 text-white fixed right-[10px] bottom-[10px] p-2 border-2 rounded-md z-10 m-2 transition-opacity ease-in-out duration-150 ${
+        showLink
+          ? "opacity-100 pointer-events-auto"
+          : "opacity-0 pointer-events-none"
       }`}
-      aria-label='Back to top'>
-      <UpArrow width={15} height={15} strokeWidth={2} stroke='#aaaaaa' />
+      aria-label="Back to top"
+    >
+      <UpArrow width={15} height={15} strokeWidth={2} stroke="#aaaaaa" />
     </Link>
   );
 };
