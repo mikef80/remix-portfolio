840e6b32699e8c0193e4f096264884f5c2173dd6
Mike <mike@mike-francis.org>
2024-11-13 21:53:27 +0000

    navbar is transparent at top of screen, then white on scroll.  still need to add dark cmode to control general colours

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,10 +6,10 @@ export default {
   theme: {
     extend: {
       flex: {
-        '1-col': '1 1 100%',
-        '2-col': '1 1 calc(100% / 2)',
-        '3-col':'1 1 calc(100% / 3)',
-        '7-col':'1 1 calc(100% / 7)'
+        "1-col": "1 1 100%",
+        "2-col": "1 1 calc(100% / 2)",
+        "3-col": "1 1 calc(100% / 3)",
+        "7-col": "1 1 calc(100% / 7)",
       },
       fontFamily: {
         raleway: ["Raleway", ...defaultTheme.fontFamily.sans],
