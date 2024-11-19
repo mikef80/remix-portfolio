8c083da07f73a03ae851596394e3504be39fa3c8
Mike <mike@mike-francis.org>
2024-11-19 20:55:29 +0000

    styled lightmode

--- app/components/BackToTop.tsx
+++ app/components/BackToTop.tsx
@@ -25,14 +25,14 @@ const BackToTop = () => {
     <Link
       id="back-to-top"
       to="#"
-      className={`border-white bg-gray-800 text-white fixed right-[10px] bottom-[10px] p-2 border-2 rounded-md z-10 m-2 transition-opacity ease-in-out duration-150 ${
+      className={`border-gray-800 bg-white text-gray-800 fixed right-[10px] bottom-[10px] p-2 border-2 rounded-md z-10 m-2 transition-opacity ease-in-out duration-150 ${
         showLink
           ? "opacity-100 pointer-events-auto"
           : "opacity-0 pointer-events-none"
       }`}
       aria-label="Back to top"
     >
-      <UpArrow width={15} height={15} strokeWidth={2} stroke="#aaaaaa" />
+      <UpArrow width={15} height={15} strokeWidth={2} stroke="stroke-gray-800" />
     </Link>
   );
 };
